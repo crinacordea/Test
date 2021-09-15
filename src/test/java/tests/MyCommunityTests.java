3477efac8a2b9d641f6e8f126add8a9470c39eb9
crinacordea <crina.cordea@gmail.com>
2021-09-15 12:57:22 +0300

    version a

--- src/test/java/tests/MyCommunityTests.java
+++ src/test/java/tests/MyCommunityTests.java
@@ -61,7 +61,7 @@ public class MyCommunityTests {
     public  void cleanUp(){
         ///driver.manage().deleteAllCookies();
        driver.quit();
-        System.out.println( "verion a");
+        System.out.println( "version a");
 
     }
 
